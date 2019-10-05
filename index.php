e216d0c651df644ef2e0cebd9fa8b6231a181709
nassau49 <56183513+nassau49@users.noreply.github.com>
2019-10-05 13:39:40 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>お問い合わせフォーム</title>
+	</head>
+	<body>
+		<form method="post" action="">
+			氏:<input type="text"name="family_name" value=""><br>
+			名:<input type="text" name="first_name" value=""><br>
+			<input type="submit" name="send" value="クリック">
+		</form>
+	</body>
+</html>
